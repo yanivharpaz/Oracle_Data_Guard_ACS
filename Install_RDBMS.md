a1c4ffeb466503049f015439b09c505193cd712b
Yaniv Harpaz <yanivharpaz@users.noreply.github.com>
2022-08-06 23:22:46 +0300

    Oracle Env

--- Install_RDBMS.md
+++ Install_RDBMS.md
@@ -49,6 +49,7 @@ sudo chmod +x ./Oracle_Data_Guard_ACS/scripts/*.sh
 ./Oracle_Data_Guard_ACS/scripts/010_yum_prep.sh
 ./Oracle_Data_Guard_ACS/scripts/030_sw_dl.sh
 ./Oracle_Data_Guard_ACS/scripts/050_install_sw.sh
+./Oracle_Data_Guard_ACS/scripts/070_setup_oraenv.sh
 
 ```
 
---
+++ scripts/070_setup_oraenv.sh
@@ -0,0 +1,19 @@
+
+sudo cp ~oracle/.bashrc /tmp/oracle_bashrc
+sudo chmod 777 /tmp/oracle_bashrc
+
+sudo cat >> /tmp/oracle_bashrc << EOF
+#aliases
+alias sss='sqlplus / as sysdba'
+alias pmon='ps -ef | grep pmon | grep -v grep'
+alias cdnet='cd $ORACLE_HOME/network/admin'
+
+#variables
+export ORACLE_HOME=/opt/oracle/product/19c/dbhome_1/
+export ORACLE_SID=ORCLCDB
+export PATH=\$PATH:\$ORACLE_HOME/bin
+EOF
+
+sudo cp -pv /tmp/oracle_bashrc ~oracle/.bashrc 
+sudo chmod 644 ~oracle/.bashrc
+
