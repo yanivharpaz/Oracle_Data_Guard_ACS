f64c21446e2c02e6e5500a584d762a1e6650748e
Yaniv Harpaz <yanivharpaz@users.noreply.github.com>
2022-08-07 02:03:27 +0300

    Echo on scripts

--- scripts/110_run_all.sh
+++ scripts/110_run_all.sh
@@ -1,5 +1,5 @@
 
-cp oracle_rdbms_config_sample.conf /tmp
+cp ~/Oracle_Data_Guard_ACS/misc/oracle_rdbms_config_sample.conf /tmp
 
 sudo chmod +x ./Oracle_Data_Guard_ACS/scripts/*.sh
 ./Oracle_Data_Guard_ACS/scripts/010_yum_prep.sh
