7898e1c24279aef47564c298650d897369722283
Yaniv Harpaz <yanivharpaz@users.noreply.github.com>
2022-08-07 17:57:56 +0300

    Echo on scripts

--- scripts/070_setup_oraenv.sh
+++ scripts/070_setup_oraenv.sh
@@ -4,14 +4,16 @@ sudo chmod 777 /tmp/oracle_bashrc
 
 sudo cat >> /tmp/oracle_bashrc << EOF
 #variables
+
 export ORACLE_HOME=/opt/oracle/product/19c/dbhome_1/
 export ORACLE_SID=ORCLCDB
+
 export PATH=\$PATH:\$ORACLE_HOME/bin
 
 #aliases
 alias sss='sqlplus / as sysdba'
 alias pmon='ps -ef | grep pmon | grep -v grep'
-alias cdnet='cd $ORACLE_HOME/network/admin'
+alias cdnet='cd \$ORACLE_HOME/network/admin'
 alias ll='ls -ltrh'
 
 EOF
