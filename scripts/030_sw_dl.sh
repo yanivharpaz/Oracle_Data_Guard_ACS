4835f44a7ae73c44851baa1c9654bd820f4936b6
Yaniv Harpaz <yanivharpaz@users.noreply.github.com>
2022-09-08 14:52:34 +0300

    added linux installation zip file

--- scripts/030_sw_dl.sh
+++ scripts/030_sw_dl.sh
@@ -4,4 +4,4 @@ cd /tmp
 curl -o oracle-database-preinstall-19c-1.0-2.el7.x86_64.rpm https://yum.oracle.com/repo/OracleLinux/OL7/latest/x86_64/getPackage/oracle-database-preinstall-19c-1.0-2.el7.x86_64.rpm
 
 wget https://stgpubus.blob.core.windows.net/file-share/files/new/oracle-database-ee-19c-1.0-1.x86_64.rpm
-
+wget https://stgpubus.blob.core.windows.net/file-share/files/LINUX.X64_193000_db_home.zip
