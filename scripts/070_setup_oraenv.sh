3a999481252912f73109ee3973c050a3d7a329d8
Yaniv Harpaz <yanivharpaz@users.noreply.github.com>
2022-10-26 17:05:35 +0300

    Update 070_setup_oraenv.sh

--- scripts/070_setup_oraenv.sh
+++ scripts/070_setup_oraenv.sh
@@ -5,8 +5,8 @@ sudo chmod 777 /tmp/oracle_bashrc
 sudo cat >> /tmp/oracle_bashrc << EOF
 #variables
 
-export ORACLE_HOME=/opt/oracle/product/19c/dbhome_1
-export ORACLE_BASE=/opt/oracle
+export ORACLE_HOME=ORACLE_HOME=/u01/app/oracle/product/19.0.0/dbhome_1
+export ORACLE_BASE=ORACLE_HOME=/u01/app/oracle
 #export ORACLE_SID=ORCLCDB
 export ORACLE_SID=CDB1
 
